5af73173b8697db8e7bf7a8f0f492de1413dc4d3
0799muhammadluthfi-spec <0799muhammadluthfi@gmail.com>
2026-04-20 23:20:00 +0800

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -899,10 +899,14 @@ def halaman_parkir(menu):
                                         safe_update("DATA_PARKIR", df_p); st.rerun()
                                 else:
                                     st.success("✅ Telah Dicetak")
-                                    
-    st.divider(); st.subheader("📊 LOG DATA TERAKHIR")
+
     if 'Tgl_Temp' in df_p.columns: df_p = df_p.drop(columns=['Tgl_Temp'])
-    st.dataframe(df_p.sort_values(by="Tanggal", ascending=False).head(15), hide_index=True)
+    with st.expander("📊 LOG INPUT TERAKHIR", expanded=False):
+        df_isi = df_p[df_p["Total_Karcis_R2"].str.strip().apply(lambda x: x not in ["-", "nan", ""])]
+        if not df_isi.empty:
+            st.dataframe(df_isi.sort_values(by="Tanggal", ascending=False).head(1), hide_index=True, use_container_width=True)
+        else:
+            st.info("Belum ada data yang diinput.")
 
 # ==========================================
 # 6. MAIN RUNNER
@@ -967,7 +971,8 @@ def main():
     if modul == "SK TOKO":
         if menu == "PENGANTARAN": halaman_pengantaran()
         else: halaman_pengambilan_sk()
-    else: halaman_parkir(menu)
+    else:
+        halaman_parkir(menu)
 
 if __name__ == "__main__":
     main()
