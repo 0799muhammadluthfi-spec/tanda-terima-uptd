355f6fb125fb4d0e69dd1fa73e61a2ec60ccabc7
0799muhammadluthfi-spec <0799muhammadluthfi@gmail.com>
2026-04-27 02:15:11 +0800

    Update 2_🅿️_Parkir.py

--- pages/2_🅿️_Parkir.py
+++ pages/2_🅿️_Parkir.py
@@ -88,7 +88,6 @@ with st.sidebar:
     st.page_link("pages/1_📋_SK_Toko.py", label="📋  SK TOKO", use_container_width=True)
     st.page_link("pages/2_🅿️_Parkir.py", label="🅿️  PARKIR", use_container_width=True)
     st.page_link("pages/3_💰_Kas.py", label="💰  KAS UPTD", use_container_width=True)
-    st.page_link("pages/4_📅_Absen.py", label="📅  ABSEN", use_container_width=True)
 
     st.markdown(
         """
